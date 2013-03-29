103b75ad93a26e964b03c9e44bd0ee864a3fb15c
Fma965 <fma96580@gmail.com>
2013-03-29 23:15:38 +0000

    Beta 1
    Initial Release

---
+++ Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.DimGray
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(80, 2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(65, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "`"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(1, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Recording Key: "
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(90, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(10, 10)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Temp Fix"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(0, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(280, 41)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Please turn off all music to be able to hear the instructions" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Copyright © 2013" & _
+    " CyanLabs (Fma965)"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.SACNR_Macro_Recorder_Beta_Test_1.My.Resources.Resources._stop
+        Me.PictureBox1.InitialImage = Global.SACNR_Macro_Recorder_Beta_Test_1.My.Resources.Resources._stop
+        Me.PictureBox1.Location = New System.Drawing.Point(240, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(16, 16)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(168, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Current State: "
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon1.BalloonTipText = "You will need to be able to hear the instructions"
+        Me.NotifyIcon1.BalloonTipTitle = "Please Stop All Music "
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "SACNR Macro Recorder - Beta Test 1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(281, 67)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "SACNR Macro Recorder - Beta Test 1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,332 @@
+﻿Imports System.Speech.Synthesis
+Public Class Form1
+    Dim speaker As New SpeechSynthesizer()
+    Private WithEvents kbHook As New kbhook
+    Dim isrecording As Boolean = False
+    Dim convertedkeycode As String = ""
+    Dim firstkey As Boolean = False
+    Dim inisettings As New ini(Application.StartupPath & "\keybinds.sav")
+    Dim keybindkey As String = ""
+    Dim macro As String = ""
+    Dim x As Integer = 0
+    Private Sub kbHook_KeyDown(ByVal Key As System.Windows.Forms.Keys) Handles kbHook.KeyDown
+        If FixKey(Key) = TextBox1.Text Then
+            If isrecording Then
+                PictureBox1.Image = My.Resources._stop
+                speaker.SpeakAsync("Macro saved. Recording stopped")
+                Timer1.Stop()
+                isrecording = False
+                inisettings.WriteString("Macros", "Macro" & x, macro)
+                x = x + 1
+                macro = ""
+                speaker.SpeakAsync("To use your new macro, Press the " & keybindkey & " key.")
+                Exit Sub
+            Else
+                isrecording = True
+                speaker.SpeakAsync("Press the key you wish to assign")
+                firstkey = True
+                PictureBox1.Image = My.Resources.record
+                Timer1.Start()
+                Exit Sub
+            End If
+        ElseIf isrecording Then
+            If Not Key = Keys.Scroll Then
+                If firstkey Then
+                    firstkey = False
+                    keybindkey = FixKey(Key).ToString
+                    inisettings.WriteString("Keys", "Key" & x, keybindkey)
+                    speaker.Speak("The " & keybindkey & " key was assigned. Recording started.")
+                Else
+                    Debug.Write(FixKey(Key))
+                    If Key = 8 Then
+                        macro = macro.Substring(0, macro.Length - 1)
+                    Else
+                        macro &= FixKey(Key)
+                    End If
+
+                    Timer1.Interval = 1000
+                End If
+            End If
+        Else
+            For i As Integer = 1 To x
+                If FixKey(Key) = inisettings.GetString("Keys", "Key" & i, "NOTHING") Then
+                    SendKeys.SendWait(inisettings.GetString("Macros", "Macro" & i, ""))
+                End If
+            Next
+        End If
+    End Sub
+    Public Function FixKey(ByVal keycode As Integer)
+        Select Case keycode
+            Case 48 To 57
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    Select Case keycode
+                        Case 48
+                            convertedkeycode = ")"
+                        Case 49
+                            convertedkeycode = "!"
+                        Case 50
+                            convertedkeycode = """"
+                        Case 51
+                            convertedkeycode = "£"
+                        Case 52
+                            convertedkeycode = "$"
+                        Case 53
+                            convertedkeycode = "%"
+                        Case 54
+                            convertedkeycode = "^"
+                        Case 55
+                            convertedkeycode = "&"
+                        Case 56
+                            convertedkeycode = "*"
+                        Case 57
+                            convertedkeycode = "("
+                    End Select
+                Else
+                    convertedkeycode = Convert.ToChar(keycode)
+                End If
+            Case 65 To 90
+                If My.Computer.Keyboard.ShiftKeyDown Or My.Computer.Keyboard.CapsLock Then
+                    convertedkeycode = Convert.ToChar(keycode)
+                Else
+                    convertedkeycode = Convert.ToChar(keycode + 32)
+                End If
+            Case 1
+                convertedkeycode = ""
+                'Case 2
+                '    convertedkeycode = "<RCLICK>"
+            Case 8
+                convertedkeycode = ""
+                'Case 9
+                '    convertedkeycode = "<TAB>"
+            Case 12
+                convertedkeycode = "5"
+            Case 13
+                convertedkeycode = "{ENTER}"
+                'Case 17
+                '    convertedkeycode = "<CTRL>"
+                'Case 18
+                '    convertedkeycode = "<ALT>"
+                'Case 19
+                '    convertedkeycode = "<PAUSE>"
+            Case 20
+                convertedkeycode = ""
+                'Case 27
+                '    convertedkeycode = "<ESC>"
+            Case 32
+                convertedkeycode = " "
+                'Case 33
+                '    convertedkeycode = "<PAGEUP>"
+                'Case 34
+                '    convertedkeycode = "<PAGEDOWN>"
+                'Case 35
+                '    convertedkeycode = "<END>"
+                'Case 36
+                '    convertedkeycode = "<HOME>"
+                'Case 37
+                '    convertedkeycode = "<LEFT>"
+                'Case 38
+                '    convertedkeycode = "<UP>"
+                'Case 39
+                '    convertedkeycode = "<RIGHT>"
+                'Case 40
+                '    convertedkeycode = "<DOWN>"
+                'Case 44
+                '    convertedkeycode = "<PRNTSCRN>"
+                'Case 45
+                '    convertedkeycode = "<INSERT>"
+                'Case 46
+                '    convertedkeycode = "<DEL>"
+                'Case 47
+                '    convertedkeycode = "<HELP>"
+                'Case 91
+                '    convertedkeycode = "<WIN>"
+                'Case 93
+                '    convertedkeycode = "<CONTEXT>"
+            Case 96
+                convertedkeycode = "0"
+            Case 97
+                convertedkeycode = "1"
+            Case 98
+                convertedkeycode = "2"
+            Case 99
+                convertedkeycode = "3"
+            Case 100
+                convertedkeycode = "4"
+            Case 101
+                convertedkeycode = "5"
+            Case 102
+                convertedkeycode = "6"
+            Case 103
+                convertedkeycode = "7"
+            Case 104
+                convertedkeycode = "8"
+            Case 105
+                convertedkeycode = "9"
+            Case 106
+                convertedkeycode = "*"
+            Case 107
+                convertedkeycode = "+"
+            Case 109
+                convertedkeycode = "-"
+            Case 110
+                convertedkeycode = "."
+            Case 111
+                convertedkeycode = "/"
+            Case 112
+                convertedkeycode = "F1"
+            Case 113
+                convertedkeycode = "F2"
+            Case 114
+                convertedkeycode = "F3"
+            Case 115
+                convertedkeycode = "F4"
+            Case 116
+                convertedkeycode = "F5"
+            Case 117
+                convertedkeycode = "F6"
+            Case 118
+                convertedkeycode = "F7"
+            Case 119
+                convertedkeycode = "F8"
+            Case 120
+                convertedkeycode = "F9"
+            Case 121
+                convertedkeycode = "F10"
+            Case 122
+                convertedkeycode = "F11"
+            Case 123
+                convertedkeycode = "F12"
+                'Case 144
+                '    convertedkeycode = "<NUMLOCK>"
+                'Case 162
+                '    convertedkeycode = "<LCTRL>"
+                'Case 163
+                '    convertedkeycode = "<RCTRL>"
+                'Case 164
+                '    convertedkeycode = "<ALT>"
+                'Case 165
+                '    convertedkeycode = "<ALTGR>"
+            Case 186
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = ":"
+                Else
+                    convertedkeycode = ";"
+                End If
+            Case 187
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "+"
+                Else
+                    convertedkeycode = "="
+                End If
+            Case 188
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "<"
+                Else
+                    convertedkeycode = ","
+                End If
+            Case 189
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "_"
+                Else
+                    convertedkeycode = "-"
+                End If
+            Case 190
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = ">"
+                Else
+                    convertedkeycode = "."
+                End If
+            Case 191
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "?"
+                Else
+                    convertedkeycode = "/"
+                End If
+            Case 192
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "@"
+                Else
+                    convertedkeycode = "'"
+                End If
+            Case 219
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "{"
+                Else
+                    convertedkeycode = "["
+                End If
+            Case 220
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "|"
+                Else
+                    convertedkeycode = "\"
+                End If
+            Case 221
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "}"
+                Else
+                    convertedkeycode = "]"
+                End If
+            Case 222
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "~"
+                Else
+                    convertedkeycode = "#"
+                End If
+            Case 223
+                If My.Computer.Keyboard.ShiftKeyDown Then
+                    convertedkeycode = "¬"
+                Else
+                    convertedkeycode = "`"
+                End If
+            Case Else
+                convertedkeycode = ""
+        End Select
+        Return convertedkeycode
+    End Function
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        If isrecording Then
+            Exit Sub
+        End If
+        If e.KeyValue = 145 Then
+            e.SuppressKeyPress = True
+            Exit Sub
+        End If
+        sender.text = FixKey(e.KeyValue)
+        Button1.Focus()
+    End Sub
+    Private Declare Sub keybd_event Lib "user32" (ByVal bVk As Byte, ByVal bScan As Byte, ByVal dwFlags As Integer, ByVal dwExtraInfo As Integer)
+    Private Const VK_SCROLL As Integer = &H91
+    Private Const KEYEVENTF_EXTENDEDKEY As Integer = &H1
+    Private Const KEYEVENTF_KEYUP As Integer = &H2
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY Or 0, 0)
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY Or KEYEVENTF_KEYUP, 0)
+        If Not firstkey Then
+            If PictureBox1.Visible Then
+                PictureBox1.Visible = False
+            Else
+                PictureBox1.Visible = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        inisettings.WriteInteger("Settings", "MacroCount", x - 1)
+        inisettings.WriteString("Settings", "RecordKey", TextBox1.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = inisettings.GetString("Settings", "RecordKey", TextBox1.Text)
+        x = inisettings.GetInteger("Settings", "MacroCount", 1)
+    End Sub
+
+    Private Sub TextBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles TextBox1.MouseDown
+        speaker.Speak("Press the key you would like to use to toggle recording.")
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        speaker.Speak("Welcome Beta Testers. Please turn off your music to be able to hear the instructions")
+    End Sub
+End Class
